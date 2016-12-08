60f9bf67d837256985f9960460d61337f8906d5f
Conlan <cjhanwel@ucalgary.ca>
2016-12-08 14:23:58 -0700

    beginning first HT

--- FFT/convolve.cpp
+++ FFT/convolve.cpp
@@ -197,8 +197,6 @@ int16_t* complexDoubleToInt (double* inArray, int size) {
     return outArray;
 }
 
-
-
 double * normalDouble (double* inArray, int size) {
     double maxValue = 0.0;
     for (int j =0; j < size; j++){
@@ -241,51 +239,49 @@ int formatP(int P) {
 //  at index 1, not 0, so subtract 1 when
 //  calling the routine (see main() below).
 
-void four1(double data[], int nn, int isign)
-{
+void four1(double data[], int nn, int isign) {
     unsigned long n, mmax, m, j, istep, i;
     double wtemp, wr, wpr, wpi, wi, theta;
     double tempr, tempi;
-
     n = nn << 1;
     j = 1;
 
     for (i = 1; i < n; i += 2) {
-	if (j > i) {
-	    SWAP(data[j], data[i]);
-	    SWAP(data[j+1], data[i+1]);
-	}
-	m = nn;
-	while (m >= 2 && j > m) {
-	    j -= m;
-	    m >>= 1;
-	}
-	j += m;
+        if (j > i) {
+            SWAP(data[j], data[i]);
+            SWAP(data[j+1], data[i+1]);
+        }
+        m = nn;
+        while (m >= 2 && j > m) {
+            j -= m;
+            m >>= 1;
+        }
+        j += m;
     }
 
     mmax = 2;
     while (n > mmax) {
-	istep = mmax << 1;
-	theta = isign * (6.28318530717959 / mmax);
-	wtemp = sin(0.5 * theta);
-	wpr = -2.0 * wtemp * wtemp;
-	wpi = sin(theta);
-	wr = 1.0;
-	wi = 0.0;
-	for (m = 1; m < mmax; m += 2) {
-	    for (i = m; i <= n; i += istep) {
-		j = i + mmax;
-		tempr = wr * data[j] - wi * data[j+1];
-		tempi = wr * data[j+1] + wi * data[j];
-		data[j] = data[i] - tempr;
-		data[j+1] = data[i+1] - tempi;
-		data[i] += tempr;
-		data[i+1] += tempi;
-	    }
-	    wr = (wtemp = wr) * wpr - wi * wpi + wr;
-	    wi = wi * wpr + wtemp * wpi + wi;
-	}
-	mmax = istep;
+        istep = mmax << 1;
+        theta = isign * (6.28318530717959 / mmax);
+        wtemp = sin(0.5 * theta);
+        wpr = -2.0 * wtemp * wtemp;
+        wpi = sin(theta);
+        wr = 1.0;
+        wi = 0.0;
+        for (m = 1; m < mmax; m += 2) {
+            for (i = m; i <= n; i += istep) {
+            j = i + mmax;
+            tempr = wr * data[j] - wi * data[j+1];
+            tempi = wr * data[j+1] + wi * data[j];
+            data[j] = data[i] - tempr;
+            data[j+1] = data[i+1] - tempi;
+            data[i] += tempr;
+            data[i+1] += tempi;
+            }
+            wr = (wtemp = wr) * wpr - wi * wpi + wr;
+            wi = wi * wpr + wtemp * wpi + wi;
+        }
+        mmax = istep;
     }
 }
 
